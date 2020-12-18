a68aafef0734ef481f190f8ba6d7572717aa7497
Hyechan Shin <kirikeria@gmail.com>
2020-12-18 17:45:27 +0200

    third edition complete

--- React/Ravenous/ravenous/src/components/SearchBar/SearchBar.js
+++ React/Ravenous/ravenous/src/components/SearchBar/SearchBar.js
@@ -4,18 +4,22 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             term: '',
             location: '',
             sortBy: 'best_match'
-        }
-        this.handleTermChange = this.handleTermChange.bind;
-        this.handleLocationChange = this.handleLocationChange.bind;
+        };
+
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        
         this.sortByOptions = {
             'Best Match': 'best_match',
             'Highest Rated': 'rating',
             'Most Reviewed': 'review_count'
-        }
+        };
     }
 
     getSortByClass(sortByOption) {
@@ -30,20 +34,25 @@ class SearchBar extends React.Component {
     handleSortByChange(sortByOption) {
         this.setState({ sortBy: sortByOption });
     }
-
+    
     handleTermChange(event) {
         this.setState({term: event.target.value});
     }
-
+    
     handleLocationChange(event) {
         this.setState({location: event.target.value});
     }
+    
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        event.preventDefault();
+    }
 
     renderSortByOptions() {
-    // The purpose of renderSortByOptions() is to dynamically create the list items needed to display the sort options (Best Match, Highest Rated, Most Reviewed). This is to help future proof against potential changes to the Yelp API.
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue} className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange.bind(this, sortByOptions)}>{sortByOption}</li>;
+        // The purpose of renderSortByOptions() is to dynamically create the list items needed to display the sort options (Best Match, Highest Rated, Most Reviewed). This is to help future proof against potential changes to the Yelp API.
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return (<li key={sortByOptionValue} className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>{sortByOption}</li>);
         });
     }
     render() {
@@ -53,11 +62,11 @@ class SearchBar extends React.Component {
                     <ul>{this.renderSortByOptions()}</ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+                    <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
                 <div className="SearchBar-submit">
-                    <a href="www.#.com">Let's Go</a>
+                    <a onClick={this.handleSearch} href="www.#.com">Let's Go</a>
                 </div>
             </div>
         )

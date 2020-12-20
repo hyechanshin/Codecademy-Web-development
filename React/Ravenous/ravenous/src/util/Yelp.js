05ff552701a3d9bf70a568f894fbb5e5a300a826
Hyechan Shin <kirikeria@gmail.com>
2020-12-20 18:12:28 +0200

    bug fix

--- React/Ravenous/ravenous/src/util/Yelp.js
+++ React/Ravenous/ravenous/src/util/Yelp.js
@@ -1,5 +1,6 @@
 const apiKey = 'FfNFug0TRu1UrTpLor_zN9NIGe9JmlmmEMHfCURdCerizsFc2LJr-1x2sqvtJdCg2hewnBNBdfT1RqvzzRE15ctLJP5JtLyeJK2LOH9UPPADdallyW9Fn7pgU2rfX3Yx';
-export const Yelp = {
+
+const Yelp = {
     search(term, location, sortBy) {
         //CORS Anywhere will take requests sent to its API endpoint, make them for the requesting app with the proper CORS permissions, and then return the response back to the requesting app.
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
@@ -10,7 +11,7 @@ export const Yelp = {
             }).then(jsonResponse => {
                 if (jsonResponse.businses) {
                     return jsonResponse.business.map(business => {
-                        id: business.id,
+                        return { id: business.id,
                         imageSrc: business.image_url,
                         name: business.name,
                         address: business.address,
@@ -20,9 +21,12 @@ export const Yelp = {
                         category: business.category,
                         rating: business.rating,
                         reviewCount: business.reviewCount
+                        }
                     });
                 }
             })
         });
     }
 }
+
+export default Yelp;

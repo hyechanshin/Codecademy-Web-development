b93076c838cb338587446f908d1dfad2609cf8a1
Hyechan Shin <kirikeria@gmail.com>
2020-12-20 17:49:19 +0200

    convert the returned response to JSON

--- React/Ravenous/ravenous/src/util/Yelp.js
+++ React/Ravenous/ravenous/src/util/Yelp.js
@@ -2,6 +2,12 @@ const apiKey = 'FfNFug0TRu1UrTpLor_zN9NIGe9JmlmmEMHfCURdCerizsFc2LJr-1x2sqvtJdCg
 const Yelp = {
     search(term, location, sortBy) {
         //CORS Anywhere will take requests sent to its API endpoint, make them for the requesting app with the proper CORS permissions, and then return the response back to the requesting app.
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }.then(response => {
+                return response.json();
+            })
+        });
     }
 }

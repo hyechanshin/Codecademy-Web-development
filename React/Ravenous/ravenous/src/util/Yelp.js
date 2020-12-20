d1c1337079d42d2b26457813a60c99eee383f0d3
Hyechan Shin <kirikeria@gmail.com>
2020-12-20 17:31:15 +0200

    add whatwg-fetch, yelp apikey

---
+++ React/Ravenous/ravenous/src/util/Yelp.js
@@ -0,0 +1,7 @@
+const apiKey = 'FfNFug0TRu1UrTpLor_zN9NIGe9JmlmmEMHfCURdCerizsFc2LJr-1x2sqvtJdCg2hewnBNBdfT1RqvzzRE15ctLJP5JtLyeJK2LOH9UPPADdallyW9Fn7pgU2rfX3Yx';
+const Yelp = {
+    search(term, location, sortBy) {
+        //CORS Anywhere will take requests sent to its API endpoint, make them for the requesting app with the proper CORS permissions, and then return the response back to the requesting app.
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+    }
+}

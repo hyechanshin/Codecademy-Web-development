4d695bd2434c9608ff88d00c3e0b02f2031754c3
Hyechan Shin <kirikeria@gmail.com>
2020-12-20 17:56:41 +0200

    Yelp module

--- React/Ravenous/ravenous/src/util/Yelp.js
+++ React/Ravenous/ravenous/src/util/Yelp.js
@@ -1,5 +1,5 @@
 const apiKey = 'FfNFug0TRu1UrTpLor_zN9NIGe9JmlmmEMHfCURdCerizsFc2LJr-1x2sqvtJdCg2hewnBNBdfT1RqvzzRE15ctLJP5JtLyeJK2LOH9UPPADdallyW9Fn7pgU2rfX3Yx';
-const Yelp = {
+export const Yelp = {
     search(term, location, sortBy) {
         //CORS Anywhere will take requests sent to its API endpoint, make them for the requesting app with the proper CORS permissions, and then return the response back to the requesting app.
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
@@ -7,6 +7,21 @@ const Yelp = {
                 Authorization: `Bearer ${apiKey}`
             }.then(response => {
                 return response.json();
+            }).then(jsonResponse => {
+                if (jsonResponse.businses) {
+                    return jsonResponse.business.map(business => {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.address,
+                        city: business.city,
+                        state: business.state,
+                        zipCode: business.zipCode,
+                        category: business.category,
+                        rating: business.rating,
+                        reviewCount: business.reviewCount
+                    });
+                }
             })
         });
     }

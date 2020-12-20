ec8615e9f9f2ea0838773d98bb804f3b1b50c9ac
Hyechan Shin <kirikeria@gmail.com>
2020-12-20 18:25:25 +0200

    finla versiongit add ravenous/

--- React/Ravenous/ravenous/src/components/App/App.js
+++ React/Ravenous/ravenous/src/components/App/App.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import './App.css';
 
-import BusinessList from './../BusinessList/BusinessList';
-import SearchBar from './../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
-import Yelp from './../../util/Yelp';
+import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
-  constructor() {
+  constructor(props) {
+    super(props);
+
     this.state = {
       businesses: []
     };
-    this.searchYelp.bind(this);
+    this.searchYelp = this.searchYelp.bind(this);
   }
+
   searchYelp(term, location, sortBy) {
-    Yelp.search(term, location, sortBy).then(business => {
-      this.setState({ business: businesses });
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({ businesses: businesses });
     })
   }
+  
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
--- React/Ravenous/ravenous/src/util/Yelp.js
+++ React/Ravenous/ravenous/src/util/Yelp.js
@@ -3,30 +3,29 @@ const apiKey = 'FfNFug0TRu1UrTpLor_zN9NIGe9JmlmmEMHfCURdCerizsFc2LJr-1x2sqvtJdCg
 const Yelp = {
     search(term, location, sortBy) {
         //CORS Anywhere will take requests sent to its API endpoint, make them for the requesting app with the proper CORS permissions, and then return the response back to the requesting app.
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-            headers: {
-                Authorization: `Bearer ${apiKey}`
-            }.then(response => {
-                return response.json();
-            }).then(jsonResponse => {
-                if (jsonResponse.businses) {
-                    return jsonResponse.business.map(business => {
-                        return { id: business.id,
-                        imageSrc: business.image_url,
-                        name: business.name,
-                        address: business.address,
-                        city: business.city,
-                        state: business.state,
-                        zipCode: business.zipCode,
-                        category: business.category,
-                        rating: business.rating,
-                        reviewCount: business.reviewCount
-                        }
-                    });
-                }
-            })
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+        headers: {
+            Authorization: `Bearer ${apiKey}`
+        }
+    }).then(response => {
+        return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => ({
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                }));
+            }
         });
     }
-}
+};
 
 export default Yelp;

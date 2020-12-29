d4637fc980885b39b323895823d86005cd842e25
Hyechan Shin <kirikeria@gmail.com>
2020-12-29 11:43:32 +0200

    Create a Method that Saves the Playlist to a User's Account

--- Jammming-React/jammming/src/Components/App/App.js
+++ Jammming-React/jammming/src/Components/App/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -44,6 +45,10 @@ class App extends React.Component {
     this.setState({ playlistName: name });
   }
 
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -52,11 +57,12 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}
-            onAdd={this.addTrack} />
+              onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} 
-            playlistTracks={this.state.playlistTracks} 
-            onRemove={this.removeTrack} 
-            onNameChange={this.updatePlaylistName} />
+              playlistTracks={this.state.playlistTracks} 
+              onRemove={this.removeTrack} 
+              onNameChange={this.updatePlaylistName} 
+              onSave={this.savePlaylist} />
           </div>
         </div>
       </div>

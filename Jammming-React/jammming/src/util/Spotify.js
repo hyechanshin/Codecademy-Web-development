0b160645aa42208bb7f16f060b8791bd70065857
Hyechan Shin <kirikeria@gmail.com>
2020-12-29 14:02:46 +0200

    Save a User's Playlist

--- Jammming-React/jammming/src/util/Spotify.js
+++ Jammming-React/jammming/src/util/Spotify.js
@@ -44,6 +44,33 @@ const Spotify = {
             URL: track.url
         }));
         })
+    },
+
+    savePlaylist(name, trackUris) {
+        if (!name || !trackUris.length) {
+            return ;
+        }
+
+        const accessToken = Spotify.getAccessToken();
+        const headers = { Authorization: `Bearer ${accessToken}` };
+        let userId;
+
+        return fetch('https://api.spotify.com/v1/me', { headers: headers }).then(response => response.json()).then(jsonResponse => {
+            userId = jsonResponse.id;
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, { 
+                headers: headers,
+                method: 'POST',
+                body: JSON.stringify({ name: name })
+            }).then(response => response.json()
+            ).then(jsonResponse => {
+                const playlist = jsonResponse.id;
+                return fetch(`/v1/users/{user_id}/playlists/{playlist_id}/tracks`, { 
+                    headers: headers,
+                    method: 'POST',
+                    body: JSON.stringify({ uris: trackUris })
+                })
+            })
+        })
     }
 }
 

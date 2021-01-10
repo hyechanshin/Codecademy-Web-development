94910ffb89470bef00cd0bce60c5702b223beeca
Hyechan Shin <kirikeria@gmail.com>
2021-01-10 15:08:17 +0200

    add checkMillionDollarIdea.js

--- Boss_Machine-NodeJS/server/checkMillionDollarIdea.js
+++ Boss_Machine-NodeJS/server/checkMillionDollarIdea.js
@@ -1,4 +1,13 @@
-const checkMillionDollarIdea = () => {};
+//This function will make sure that any new or updated ideas are still worth at least one million dollars! The total value of an idea is the product of its numWeeks and weeklyRevenue properties.
+const checkMillionDollarIdea = (req, res, next) => {
+    const { numWeeks, weeklyRevenue } = req.body;
+    const totalMoney = Number(numWeeks) * Number(weeklyRevenue);
+    if (!numWeeks || !weeklyRevenue || isNaN(totalMoney) || totalMoney < 1000000) {
+        res.status(404).send();
+    } else {
+        next();
+    }
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
